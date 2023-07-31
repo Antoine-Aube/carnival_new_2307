54e1d1259452411f8aed142e448ac0190a0cf42f
Antoine Aube <aaube3@gmail.com>
2023-07-31 11:56:17 -0600

    test: total revenues for all carnivals in carnival class

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe Carnival do
   let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
   let(:ride2) { Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle }) }
   let(:ride3) { Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling }) }
+  let(:ride4) { Ride.new({ name: 'Super Man', min_height: 54, admission_fee: 5, excitement: :thrilling }) }
+  let(:ride5) { Ride.new({ name: 'Log Flume', min_height: 54, admission_fee: 10, excitement: :thrilling }) }
   let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
   let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
   let(:visitor3) { Visitor.new('Penny', 64, '$15') }
   let(:visitor4) { Visitor.new('Abdul', 72, '$100') }
   let(:carnival) {  Carnival.new(14)}
-
+  let(:carnival_2) {  Carnival.new(18)}
+  
   describe "it exists and has attributes" do 
     it{ expect(carnival).to be_a Carnival }
     it{ expect(carnival.duration).to eq(14) }
@@ -58,7 +61,7 @@ RSpec.describe Carnival do
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
-      ride2.board_rider(visitor4)
+      ride2.board_rider(visitor4) #Abdul is a big fan of the Ferris Wheel"
       
       ride3.board_rider(visitor3)
       ride3.board_rider(visitor4)
@@ -97,7 +100,7 @@ RSpec.describe Carnival do
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
-      ride2.board_rider(visitor4) #Abdul is a big fan of the Ferris Wheel"
+      ride2.board_rider(visitor4) 
       
       ride3.board_rider(visitor3)
       ride3.board_rider(visitor4)
@@ -131,5 +134,42 @@ RSpec.describe Carnival do
       # require 'pry';binding.pry
       expect(carnival.total_revenue).to eq(21)
     end 
+  end
+
+  xdescribe "total_revenues" do
+    it "return the revenues of all carnivals" do 
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor4.add_preference(:gentle)
+      visitor3.add_preference(:thrilling)
+      visitor4.add_preference(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor4)
+
+      ride3.board_rider(visitor3)
+      ride3.board_rider(visitor4)
+      # require 'pry';binding.pry
+      expect(carnival.total_revenue).to eq(21)
+
+      #new carnival, minimal rides just to test class method
+      carnival_2.add_ride(ride4)
+      carnival_2.add_ride(ride5)
+
+      ride4.board_rider(visitor4)
+      ride5.board_rider(visitor4)
+
+      expect(carnival_2.total_revenue).to eq(15)
+      exepct(Carnival.total_revenues).to eq(36)
+    end
   end
 end
