04f3746968433714b3473de5b922df58282e2d7b
Antoine Aube <aaube3@gmail.com>
2023-07-31 10:33:38 -0600

    test: Carnival object exists and has attributes

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,18 @@
+require './lib/visitor'
+require './lib/ride'
+require './lib/carnival'
+
+RSpec.describe Carnival do 
+  let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
+  let(:ride2) { Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle }) }
+  let(:ride3) { Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling }) }
+  let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
+  let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
+  let(:visitor3) { Visitor.new('Penny', 64, '$15') }
+  let(:carnival) {  Carnival.new(14)}
+
+  describe "it exists and has attributes" do 
+    it{ expect(carnival).to be_a Carnival }
+    it{ expect(carnival.duration).to eq(14) }
+  end
+end
