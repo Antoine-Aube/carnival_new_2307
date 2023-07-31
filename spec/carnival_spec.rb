0d78de56c08b3bc2895f9055a1022ad868b86eac
Antoine Aube <aaube3@gmail.com>
2023-07-31 11:17:45 -0600

    test: #total_revenue for Carnival class

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -97,12 +97,38 @@ RSpec.describe Carnival do
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
       ride2.board_rider(visitor4)
-      ride2.board_rider(visitor4)
+      ride2.board_rider(visitor4) #Abdul is a big fan of the Ferris Wheel"
       
       ride3.board_rider(visitor3)
       ride3.board_rider(visitor4)
-
+      
       expect(carnival.most_profitable_ride).to eq(ride2)
     end 
+  end
+
+  describe "total_revenue" do 
+    it "returns the total revenue for the carnival of all based on ride revenue" do 
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor4.add_preference(:gentle)
+      visitor3.add_preference(:thrilling)
+      visitor4.add_preference(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor4)
+
+      ride3.board_rider(visitor3)
+      ride3.board_rider(visitor4)
+
+      expect(carnival.total_revenue).to eq(21)
   end
 end
