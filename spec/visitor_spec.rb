c5cfb8b89c2c29f5cb95374d88f21b66dd4aa7ee
Antoine Aube <aaube3@gmail.com>
2023-07-31 09:12:11 -0600

    feat: Visitor class intialized with attributes

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -13,6 +13,6 @@ RSpec.describe Visitor do
     it { expect(visitor1.name).to eq("Bruce") }
     it { expect(visitor1.height).to eq(54) }
     it { expect(visitor1.spending_money).to eq('$10') }
-    it { expect(visitor1.visitor1.preferences).to eq([]) }
+    it { expect(visitor1.preferences).to eq([]) }
   end
 end
