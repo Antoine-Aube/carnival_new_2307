064359f81cc4e70f1b4277b1fe3a0ec19e464937
Antoine Aube <aaube3@gmail.com>
2023-07-31 10:49:42 -0600

    test: #most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Carnival do
   let(:visitor1) { Visitor.new('Bruce', 54, '$10') }
   let(:visitor2) { Visitor.new('Tucker', 36, '$5') }
   let(:visitor3) { Visitor.new('Penny', 64, '$15') }
+  let(:visitor4) { Visitor.new('Abdul', 72, '$100') }
   let(:carnival) {  Carnival.new(14)}
 
   describe "it exists and has attributes" do 
@@ -24,6 +25,41 @@ RSpec.describe Carnival do
       carnival.add_ride(ride3)
 
       expect(carnival.rides).to eq([ride1, ride2, ride3])
+    end
+  end
+
+  describe "#most_popular ride" do 
+    it "can return ride object that has been riden the most" do 
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)
+
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor4.add_preference(:gentle)
+      visitor3.add_preference(:thrilling)
+      visitor4.add_preference(:thrilling)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor3)
+      ride1.board_rider(visitor3)
+      ride1.board_rider(visitor4)
+      ride1.board_rider(visitor4)
+      ride1.board_rider(visitor4)
+      ride1.board_rider(visitor4)
+      ride1.board_rider(visitor4)
+      
+      ride2.board_rider(visitor1)
+      ride2.board_rider(visitor2)
+      ride2.board_rider(visitor4)
+
+      ride3.board_rider(visitor3)
+      ride3.board_rider(visitor4)
+
+      expect(carnival.most_popular_ride).to eq(ride1)
     end
   end
 end
